8002ada720c1f1cc2856a4a9c79206456bb658f5
bernice <bernice@hubtel.com>
2023-01-26 19:32:56 +0000

    removed_db

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-// import { db } from "@/firebase";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 
 export const authOptions = {
